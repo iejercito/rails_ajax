868025fc1d20cd8fc0e8339712dda087e1cbe497
ip ejercito <iejercito@village88.com>
2013-07-26 11:11:18 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AjaxDiv::Application.config.secret_key_base = 'f8675161fd0bd37c2001463898492e191caf1a05b2241e341b4d15fec01cb6ae2d690294388870200ee24b975efd146271cd832d83769eca04fd8de66befd5a8'
